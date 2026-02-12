4c6af1441f6dd7b58903cf2c3e728f3074e94ba8
sascha-lr <174216621+sascha-lr@users.noreply.github.com>
2026-02-12 14:09:08 +0100

    Add several features
    Added several features, such as drawing while holding down mouse button,
    color picker, rainow button, clear button, grid size slider. However,
    some bits are a little messy and need refactoring.

--- app.js
+++ app.js
@@ -1,31 +1,119 @@
-const body = document.querySelector("body");
+const gridContainer = document.querySelector("#grid-container");
+const gridSizeRangeSlider = document.querySelector("#grid-size-range");
+const gridSizeText = document.querySelector("#grid-size-text");
+const colorPicker = document.querySelector("#color-picker");
+const rainbowButton = document.querySelector("#rainbow-button");
+const darkenButton = document.querySelector("#darken-button");
+const clearButton = document.querySelector("#clear-button");
 
-const maxGridWidthHeight = 32;
-const divRows = [];
-const divsInRow = [];
+function drawGrid(gridSize) {
 
-createGrid(32);
+    const maxGridWidthHeight = 80;
+    let divRows = [];
+    let divsInRow = [];
 
-const squares = document.querySelectorAll(".square");
-
-function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         divRows.push(document.createElement("div"));
         divRows[i].style.display = "flex";
-        body.appendChild(divRows[i]);
+        gridContainer.appendChild(divRows[i]);
         for (let j = 0; j < gridSize; j++) {
             divsInRow.push(document.createElement("div"));
-            divsInRow[divsInRow.length-1].style.width = `${maxGridWidthHeight/gridSize}rem`;
-            divsInRow[divsInRow.length-1].style.height = `${maxGridWidthHeight/gridSize}rem`;
-            divsInRow[divsInRow.length-1].style.border = "solid 1px black";
+            divsInRow[divsInRow.length-1].style.cssText =`
+            width: ${maxGridWidthHeight/gridSize}vw;
+            max-width: ${maxGridWidthHeight/gridSize/1.25}rem;
+            height: ${maxGridWidthHeight/gridSize}vw;
+            max-height: ${maxGridWidthHeight/gridSize/1.25}rem;
+            border: solid 1px black;`;
             divsInRow[divsInRow.length-1].classList.add("square");
             divRows[i].appendChild(divsInRow[divsInRow.length-1]);
         }
     }
+    divRows = [];
+    divsInRow = [];
 }
 
-squares.forEach(square => {
-    square.addEventListener("mouseover", (e) => {
-        square.style.backgroundColor = "black";
+function removeGrid() {
+    while (gridContainer.firstElementChild) {
+        gridContainer.removeChild(gridContainer.lastElementChild);
+    }
+}
+
+function randomNumberUpTo(number) {
+    return Math.floor(Math.random() * number+1);
+}
+
+function isRainbowButtonClicked() {
+    let rainbowButtonClicks = 0;
+    rainbowButton.addEventListener("click", () => {
+        if (rainbowButton > 1) rainbowButtonClicks = 0;
+        rainbowButtonClicks++;
     })
-});
+    if (rainbowButtonClicks % 2 === 0) return true;
+    return false;
+}
+
+function draw() {
+
+    let color = colorPicker.value;
+    colorPicker.addEventListener("input", () => {
+    color = colorPicker.value;
+    })
+
+    let holding = false;
+
+    gridContainer.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        holding = true;
+    });
+    gridContainer.addEventListener("mouseup", () => holding = false);
+    gridContainer.addEventListener("mouseleave", () => holding = false);
+
+    squares.forEach(square => {
+        square.addEventListener("mouseover", (e) => {
+            if (holding === true) {
+                if (isRainbowButtonClicked()) {
+                    square.style.backgroundColor = color;
+                } else {
+                    e.currentTarget.style.backgroundColor = `rgb(${randomNumberUpTo(255)}, ${randomNumberUpTo(255)}, ${randomNumberUpTo(255)})`;
+                }
+            }
+        })
+    });
+}
+
+function changeGridSize() {
+    gridSizeRangeSlider.addEventListener("input", function() {
+    gridSize = this.value;
+    gridSizeText.innerText = `${gridSize} x ${gridSize}`;
+    })
+
+    gridSizeRangeSlider.addEventListener("mouseup", function() {
+    removeGrid();
+    drawGrid(gridSize);
+    draw();
+    clear();
+    })
+}
+
+function clear() {
+    const squares = document.querySelectorAll(".square")
+    clearButton.addEventListener("click", () => {
+        squares.forEach(square => {
+            square.style.backgroundColor = "white";
+        });
+    })
+}
+
+function createGrid() {
+    let gridSize = gridSizeRangeSlider.value;
+    drawGrid(gridSize);
+    const squares = document.querySelectorAll(".square");
+    draw();
+    gridSizeText.innerText = `${gridSize} x ${gridSize}`;
+
+    changeGridSize();
+    clear();
+}
+
+createGrid();
+
